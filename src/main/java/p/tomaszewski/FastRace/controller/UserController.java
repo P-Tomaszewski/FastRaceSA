61861b2d40df43563c9f9102df4c2e90de9fdd09
Patryk <ptomaszewskig3@gmail.com>
2021-01-29 00:19:00 +0100

    logowanie

---
+++ src/main/java/p/tomaszewski/FastRace/controller/UserController.java
@@ -0,0 +1,27 @@
+package p.tomaszewski.FastRace.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+import p.tomaszewski.FastRace.model.User;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.Base64;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+
+    @RequestMapping("/login")
+    public boolean login(@RequestBody User user) {
+        return user.getUserName().equals("user") && user.getPassword().equals("password");
+    }
+
+    @RequestMapping("/user")
+    public Principal user(HttpServletRequest request) {
+        String authToken = request.getHeader("Authorization")
+                .substring("Basic".length()).trim();
+        return () ->  new String(Base64.getDecoder()
+                .decode(authToken)).split(":")[0];
+    }
+}
---
+++ src/main/java/p/tomaszewski/FastRace/model/User.java
@@ -0,0 +1,25 @@
+package p.tomaszewski.FastRace.model;
+
+public class User {
+    String userName;
+    String password;
+
+    public User() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
